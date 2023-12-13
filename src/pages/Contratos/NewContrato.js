03e633914b73779ac27339ef3247d0f721a81ab2
sebi <seeeps884@gmail.com>
2023-12-12 23:27:14 -0300

    ADD go to contratos

--- src/pages/Contratos/NewContrato.js
+++ src/pages/Contratos/NewContrato.js
@@ -111,7 +111,7 @@ const NewContrato = () => {
                 icon: 'success',
                 willClose: () => {
                     setTimeout(() => {
-                 //       goToContratos();
+                       goToContratos();
                     }, 250);
                 }
             })
